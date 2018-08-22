9f38832409dd70a582a493ac879073cc83f5be0f
Cong Zhang <dancerphil1994@gmail.com>
2018-08-22 15:25:09 +0800

    several deprecated warnings

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import 'babel-polyfill';
-import { asyncLoad, load } from './load';
+import { asyncLoad as rawAsyncLoad, load } from './load';
 import { mapResultToProps, getLoading, getResults, getFetchTimes } from './util/getThingsFromState';
-import { getReducer } from './reducer';
-import { setConfig as rawSetConfig } from './util/config';
+import { reducer, getReducer as rawGetReducer } from './reducer';
+import { setConfig } from './util/config';
 
-const setConfig = (config) => {
-  console.warn('setConfig is deprecated, just pass config through getReducer');
-  rawSetConfig(config);
+const asyncLoad = (dispatch, getState, key, Promise, props) => {
+  console.warn('asyncLoad is deprecated, use load instead');
+  rawAsyncLoad(dispatch, getState, key, Promise, props);
 };
 
-export { asyncLoad, load, mapResultToProps, getLoading, getResults, getFetchTimes, getReducer, setConfig };
+const getReducer = (config) => {
+  console.warn('getReducer is deprecated, use reducer instead');
+  rawGetReducer(config);
+};
+
+export { asyncLoad, load, mapResultToProps, getLoading, getResults, getFetchTimes, reducer, getReducer, setConfig };

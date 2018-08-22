ad1575bc9c5a3621565c25ae0fbe4b92ac4866fc
Cong Zhang <dancerphil1994@gmail.com>
2018-08-22 16:40:08 +0800

    remove surrounded dispatch

--- src/load.js
+++ src/load.js
@@ -1,22 +1,21 @@
 import { getResults as getSnapshot } from './util/getThingsFromState';
 import preCommit from './preCommit';
 import commit from './commit';
+import { store } from './util/config';
 
 /**
  * @param props.params Promise may need
  * @param props.format A pure function format result to other data structure
  * @param props.forceUpdate 'always' | 'need' | 'never'
  */
-async function asyncLoad(dispatch, getState, key, Promise, props = {}) {
-  if (typeof dispatch !== 'function' || typeof getState !== 'function') {
-    throw Error('dispatch and getState is required when you use asyncLoad()');
+export async function load(key, Promise, props = {}) {
+  if (!store || typeof store.dispatch !== 'function' || typeof store.getState !== 'function') {
+    throw Error('setConfig({ store }) must be called');
   }
+  const { dispatch, getState } = store;
 
   const snapshot = getSnapshot(getState(), key);
   const result = await preCommit(dispatch, getState, key, Promise, snapshot, props);
   commit(dispatch, getState, key, result, snapshot, props);
+  return result;
 }
-
-export const load = (key, Promise, props) => (dispatch, getState) => {
-  asyncLoad(dispatch, getState, key, Promise, props);
-};

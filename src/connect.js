2586bcd896dd549b889ff51ff6068758da17b43a
Cong Zhang <dancerphil1994@gmail.com>
2018-08-27 14:55:43 +0800

    wrapped connect

---
+++ src/connect.js
@@ -0,0 +1,9 @@
+import { connect as rawConnect } from 'react-redux';
+import { mapResultToProps } from './util/getThingsFromState';
+
+export const connect = (mapStateToProps, mapDispatchToProps, mergeProps, options) => {
+  if (typeof mapStateToProps === 'string') {
+    return rawConnect(mapResultToProps(mapStateToProps), mapDispatchToProps, mergeProps, options);
+  }
+  return rawConnect(mapStateToProps, mapDispatchToProps, mergeProps, options);
+};

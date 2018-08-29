37b4318fc34ee2ae826353702a4ee336148af3d9
Cong Zhang <dancerphil1994@gmail.com>
2018-08-29 10:22:49 +0800

    forceUpdate: true | false & deprecate willSetResult && didSetResult

--- src/connect.js
+++ src/connect.js
@@ -2,7 +2,7 @@ import { connect as rawConnect } from 'react-redux';
 import { mapResultToProps } from './util/getThingsFromState';
 
 export const connect = (mapStateToProps, mapDispatchToProps, mergeProps, options) => {
-  if (typeof mapStateToProps === 'string') {
+  if (typeof mapStateToProps === 'string' || Array.isArray(mapStateToProps)) {
     return rawConnect(mapResultToProps(mapStateToProps), mapDispatchToProps, mergeProps, options);
   }
   return rawConnect(mapStateToProps, mapDispatchToProps, mergeProps, options);

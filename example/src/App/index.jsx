aa792b1f13f907d602624be547aa06f828490ca4
Cong Zhang <dancerphil1994@gmail.com>
2019-01-07 12:03:01 +0800

    new example

--- example/src/App/index.jsx
+++ example/src/App/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Provider } from './Provider';
+import { Provider } from 'redux-loadings';
 import Layout from './Layout';
 import { loadFollower, loadUser } from '../shared/load';
 

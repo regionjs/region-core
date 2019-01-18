c1d29f4a452d2448f62c8a7a25a704bf7d0dda4e
Cong Zhang <dancerphil1994@gmail.com>
2019-01-18 19:24:56 +0800

    FormAdapter

--- example/src/BindForm/region.jsx
+++ example/src/BindForm/region.jsx
@@ -1,53 +1,28 @@
 import React from 'react';
 import { Region } from 'region-shortcut';
-import { Input, Checkbox, Switch, Radio } from 'antd';
 import { omit } from 'lodash';
+import antdAdapter from './adapter';
 
 class RegionForm extends Region {
   constructor() {
     super('bindForm');
+    this.adapter = antdAdapter;
   }
 
-  handleChangeFactory = key => (value) => {
-    this.set(key, value);
-  }
-
-  handleEventFactory = key => (e) => {
-    this.set(key, e.target.value);
-  }
-
-  getHandlers = (key, Component) => {
-    const { handleChangeFactory, handleEventFactory } = this;
-    const handleChange = handleChangeFactory(key);
-    const handleEvent = handleEventFactory(key);
-    const standardChangeCallback = { onChange: handleChange };
-    const standardEventCallback = { onChange: handleEvent };
-    switch (Component) {
-      case Input:
-      case Radio.Group:
-        return ['value', standardEventCallback];
-      case Switch:
-        return ['value', standardChangeCallback];
-      case Checkbox.Group:
-        return ['checked', standardChangeCallback];
-      default: {
-        const name = Component && Component.name;
-        console.warn(`Can not unrecognized Component ${name || 'Unknown'}, data is bind on props.value and callback is bind on props.onChange`); // eslint-disable-line no-console
-        return ['value', standardChangeCallback];
-      }
-    }
+  handlerFactory = (key, selector) => (value) => {
+    this.set(key, selector(value));
   }
 
   bindWith = (key, Component) => {
-    const { connectWith, getHandlers } = this;
-    const [valueName, handlers] = getHandlers(key, Component);
+    const { connectWith, handlerFactory, adapter } = this;
+    const [valueName, handlerName, selector] = adapter(Component);
+    const handler = handlerFactory(key, selector);
     const Hoc = ({ [key]: value, ...args }) => {
-      const passByArgs = omit(args, ['value', 'loading', 'error', 'dispatch'].concat(Object.keys(handlers)));
-      const valueObj = { [valueName]: value };
-      return <Component {...valueObj} {...handlers} {...passByArgs} />;
+      const passByArgs = omit(args, ['loading', 'error', 'dispatch', valueName, handlerName]);
+      const bindObj = { [valueName]: value, [handlerName]: handler };
+      return <Component {...bindObj} {...passByArgs} />;
     };
     return connectWith(key, Hoc);
   }
 }
-
 export default RegionForm;

5a3fcfc2bc1996ac1669d5de6c8298a64dc82d2d
Cong Zhang <dancerphil1994@gmail.com>
2019-02-28 16:15:44 +0800

    test load

--- src/region/__test__/region.js
+++ src/region/__test__/region.js
@@ -1,8 +1,7 @@
-import { createStore } from 'redux';
 import Region from '../index';
-import { setStore } from '../../global/store';
+import { getProvider } from '../..';
 
-setStore(createStore(() => {}));
+getProvider();
 const region = new Region({ name: null });
 
 export { region };
--- src/region/__test__/set-load.test.js
+++ src/region/__test__/set-load.test.js
@@ -1,50 +1,33 @@
 import { region } from './region';
-import { setStore } from '../../global/store';
 
 const { set, load } = region;
 
-let journey = [];
-setStore({
-  dispatch: ({ payload }) => journey.push(payload),
-  getState: () => {},
-});
-
-const getJourney = () => {
-  const ans = journey;
-  journey = [];
-  return ans;
-};
-
 describe('set', () => {
   test('string', () => {
-    set('user', 'a user');
-    expect(getJourney()).toEqual([{
-      key: 'user',
-      result: 'a user',
-    }]);
+    const result = set('user', 'a user');
+    expect(result).toBe('a user');
   });
 
   test('array', () => {
-    const result = [{ id: 1, name: 'zhangcong' }, { id: 2, name: 'milly' }];
-    set('user', result);
-    expect(getJourney()).toEqual([{
-      key: 'user',
-      result,
-    }]);
+    const target = [{ id: 1, name: 'zhangcong' }, { id: 2, name: 'milly' }];
+    const result = set('user', target);
+    expect(result).toBe(target);
   });
 
   test('function', () => {
-    const result = () => 'should not be string';
-    set('user', result);
-    expect(getJourney()).toEqual([{
-      key: 'user',
-      result,
-    }]);
+    const target = () => 'should not be string';
+    const result = set('user', target);
+    expect(result).toBe(target);
   });
 });
 
 
 describe('load', () => {
+  test('fallback to set', async () => {
+    const result = await load('user', 'set a user');
+    expect(result).toBe('set a user');
+  });
+
   test('promise', async () => {
     const result = await load('user', Promise.resolve('a user'));
     expect(result).toBe('a user');
@@ -53,5 +36,25 @@ describe('load', () => {
   test('asyncFunction', async () => {
     const result = await load('user', () => Promise.resolve('another user'));
     expect(result).toBe('another user');
+  });
+
+  test('format', async () => {
+    const result = await load('user', () => Promise.resolve('0'), {
+      format: user => `${user}1`,
+    });
+    expect(result).toBe('01');
+  });
+
+  test('format snapshot', async () => {
+    const result = await load('user', () => Promise.resolve('2'), {
+      format: (user, snapshot) => `${snapshot}${user}3`,
+    });
+    // This is extremely weird
+    expect(result).toBe('undefined23');
+  });
+
+  test('reject', async () => {
+    const result = await load('user', () => Promise.reject(new Error('2')));
+    expect(result).toBe(undefined);
   });
 });

8bc7006e8220393264466b4e8365c04144aa70ae
Cong Zhang <dancerphil1994@gmail.com>
2023-12-05 11:47:10 +0800

    Upgrade deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,6 @@ require('@reskript/config-lint/patch');
 
 module.exports = {
     extends: require.resolve('@reskript/config-lint/config/eslint'),
-    parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-    },
     rules: {
         // close some rules
         'camelcase': 'off',
